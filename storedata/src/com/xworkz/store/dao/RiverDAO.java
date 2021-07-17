680d2174542dd52b8c7327f1a04ded8a4530fdb0
darshan-2204 <83104026+darshan-2204@users.noreply.github.com>
2021-07-17 10:07:43 +0530

    store_data

--- storedata/src/com/xworkz/store/dao/RiverDAO.java
+++ storedata/src/com/xworkz/store/dao/RiverDAO.java
@@ -1,5 +1,15 @@
 package com.xworkz.store.dao;
 
-public interface RiverDAO implements RiverDTO{
+import com.xworkz.store.dto.RiverDTO;
+
+public interface RiverDAO {
+
+	boolean save(RiverDTO dto);
+
+	int totalItems();
+
+	void update(RiverDTO dto);
+
+	boolean delete(RiverDTO dto);
 
 }
